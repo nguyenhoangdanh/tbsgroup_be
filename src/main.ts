46a5427953dcfc4c6f2990df2215598b7f1c8faf
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2025-06-14 23:30:55 +0700

    added frontend url

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     app.enableCors({
       origin: [
         'http://localhost:3000',
-        'https://nmtxts-daily-performance.vercel.app',
+        'https://thoaison-handbag-factory.vercel.app',
       ],
       credentials: true,
       allowedHeaders: [
